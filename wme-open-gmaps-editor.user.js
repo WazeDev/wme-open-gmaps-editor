00202eb702945be31e17edb0e2162a13757e1d95
Gavin Canon-Phratsachack <37220586+gncnpk@users.noreply.github.com>
2025-06-25 12:11:35 -0500

    Update wme-open-gmaps-editor.user.js

--- wme-open-gmaps-editor.user.js
+++ wme-open-gmaps-editor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Open GMaps Editor
 // @namespace    https://github.com/WazeDev/wme-open-gmaps-editor
-// @version      0.0.3
+// @version      0.0.4
 // @description  Opens the Google Maps editor based on WME's map center.
 // @author       Gavin Canon-Phratsachack (https://github.com/gncnpk)
 // @match        https://beta.waze.com/*editor*
@@ -19,15 +19,15 @@
     'use strict';
     let wmeSdk;
     const zoomLevels = {
-        1:  52428800,
-        2:  26214400,
-        3:  13107200,
-        4:  6553600,
-        5:  3276800,
-        6:  1638400,
-        7:  819200,
-        8:  409600,
-        9:  204800,
+        1: 52428800,
+        2: 26214400,
+        3: 13107200,
+        4: 6553600,
+        5: 3276800,
+        6: 1638400,
+        7: 819200,
+        8: 409600,
+        9: 204800,
         10: 102400,
         11: 51200,
         12: 25600,
@@ -42,20 +42,60 @@
         21: 50,
         22: 25
     }
+
+    function waitForElm(selector) {
+        return new Promise(resolve => {
+            if (document.querySelector(selector)) {
+                return resolve(document.querySelector(selector));
+            }
+
+            const observer = new MutationObserver(mutations => {
+                if (document.querySelector(selector)) {
+                    observer.disconnect();
+                    resolve(document.querySelector(selector));
+                }
+            });
+
+            // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
+        });
+    }
     window.SDK_INITIALIZED.then(initialize)
     async function initialize() {
         wmeSdk = await getWmeSdk({
             scriptId: 'wme-open-gmaps-editor',
             scriptName: 'WME Open GMaps Editor'
         })
-        wmeSdk.Events.on({eventHandler: replaceGoogleLink, eventName: "wme-map-move-end"})
-        wmeSdk.Events.on({eventHandler: replaceGoogleLink, eventName: "wme-map-zoom-changed"})
-        replaceGoogleLink();
-     }
+        wmeSdk.Events.on({
+            eventHandler: replaceGoogleLink,
+            eventName: "wme-map-move-end"
+        })
+        wmeSdk.Events.on({
+            eventHandler: replaceGoogleLink,
+            eventName: "wme-map-zoom-changed"
+        })
+        let GMELink = document.createElement("a");
+        GMELink.id = "wme-open-gmaps-editor-url";
+        GMELink.target = "_blank";
+        let img = document.createElement("img");
+        img.src = "https://upload.wikimedia.org/wikipedia/commons/a/aa/Google_Maps_icon_%282020%29.svg";
+        img.style = "margin-right: 12px;margin-left: 12px;width: 16px;";
+        img.title = "Open in Google Maps Editor (GME)"
+        GMELink.appendChild(img);
+        waitForElm('.secondary-toolbar-actions').then((elm) => {
+            let toolbar = document.getElementsByClassName("secondary-toolbar-actions")[0];
+            toolbar.insertBefore(GMELink, toolbar.children[0]);
+            replaceGoogleLink();
+        });
+    }
+
     function replaceGoogleLink() {
         const coords = wmeSdk.Map.getMapCenter();
         const mapZoom = wmeSdk.Map.getZoomLevel();
         let url = `https://www.google.com/maps/place/@${coords.lat},${coords.lon},${zoomLevels[mapZoom]}m/data=!10m2!1e3!2e14!5m1!1e1`
-        document.getElementsByClassName("wz-map-ol-control-google-map-permalink")[0].children[0].href = url;
+        document.getElementById("wme-open-gmaps-editor-url").href = url;
     }
 })();
